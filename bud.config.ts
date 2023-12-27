033dcfb3d5d853f4370e072c87e29ff6a1b5c33a
Niklas Schönberg <niklas@uplink.fi>
2023-12-27 17:52:22 +0200

    Some attempted fixes.

--- bud.config.ts
+++ bud.config.ts
@@ -2,6 +2,8 @@ import type { Bud } from "@roots/bud";
 
 export default async (bud: Bud) => {
   bud.runtime(false);
+  bud.hash(false);
+  bud.devtool(false);
   bud.setPath("@src", "src");
   bud.entry("theme", ["./js/theme.ts", "./css/theme.css"]);
   bud.entry("admin", ["./js/admin.ts", "./css/admin.css"]);
