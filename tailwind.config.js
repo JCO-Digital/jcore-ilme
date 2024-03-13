7538fc3d61cd93c69882c3f9a6e5a7ec92791955
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-03-13 13:05:26 +0200

    feat(tailwind): Setup a safelist of classes that would be used in Gutenberg. ✨
    Closes #13

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,33 @@
-import { readFileSync, existsSync } from "node:fs";
+import {existsSync, readFileSync} from "node:fs";
 
 const colors = {};
 if (existsSync("theme.json")) {
-	const theme = JSON.parse(readFileSync("theme.json", "utf8"));
-	for (const color of theme.settings.color.palette) {
-		colors[color.slug] = color.color;
-	}
+    const theme = JSON.parse(readFileSync("theme.json", "utf8"));
+    for (const color of theme.settings.color.palette) {
+        colors[color.slug] = color.color;
+    }
 }
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./views/**/*.twig", "./src/js/**/*.js"],
-	theme: {
-		extend: {},
-		colors,
-	},
-	plugins: [],
+    content: ["./views/**/*.twig", "./src/js/**/*.js"],
+    theme: {
+        extend: {},
+        colors,
+    },
+    safelist: [
+        {
+            pattern: /^([mp][tbrlxy]?)-.+$/,
+            variants: ["sm", "md", "lg", "xl"],
+        },
+        {
+            pattern: /^((inline-)?(flex|hidden|block|grid))$/,
+            variants: ["sm", "md", "lg", "xl"],
+        },
+        {
+            pattern: /^(justify|items|align|self)-.+$/,
+            variants: ["sm", "md", "lg", "xl"],
+        }
+    ],
+    plugins: [],
 };
