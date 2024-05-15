9359317e8356ffc5cdee19741f68b1222a3b35a0
Maxemilian Grönblom <maxemiliang@gmail.com>
2024-05-15 14:43:55 +0300

    feat(mixins): added breakpoint mixins ✨

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,25 @@
 import postcssPresetEnv from "postcss-preset-env";
 import postcssImport from "postcss-import";
 import postcssMixins from "postcss-mixins";
-import postcssMinify from "postcss-minify";
+import postcssMediaMinmax from "postcss-media-minmax";
+import postcssSimpleVars from "postcss-simple-vars";
 import tailwindcss from "tailwindcss";
 import nesting from "tailwindcss/nesting/index.js";
+import { existsSync, readFileSync } from "node:fs";
+
+const colors = {};
+const breakpoints = {};
+if (existsSync("theme.json")) {
+	const theme = JSON.parse(readFileSync("theme.json", "utf8"));
+	for (const color of theme.settings.color.palette) {
+		colors[`jcore-colors-${color.slug}`] = color.color;
+	}
+	if ("breakpoints" in theme.settings.custom) {
+		for (const breakpoint of Object.keys(theme.settings.custom.breakpoints)) {
+			breakpoints[`jcore-breakpoint-${breakpoint}`] = theme.settings.custom.breakpoints[breakpoint];
+		}
+	}
+}
 
 export default {
 	map: {
@@ -12,9 +28,15 @@ export default {
 	plugins: [
 		postcssImport,
 		postcssMixins,
+		postcssMediaMinmax,
+		postcssSimpleVars({
+			variables: {
+				...colors,
+				...breakpoints,
+			}
+		}),
 		nesting,
 		tailwindcss,
 		postcssPresetEnv({ stage: 2, features: { "nesting-rules": false } }),
-		//postcssMinify,
 	],
 };
