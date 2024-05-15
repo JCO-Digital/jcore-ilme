ab643c88fc4bcc1593427a4cae76b342cecd0ac9
Linus <liinus.wiberg@gmail.com>
2024-05-15 14:03:48 +0300

    Feat: Navigation stuff

--- includes/customizer.php
+++ includes/customizer.php
@@ -29,3 +29,41 @@ Customizer::add_section(
 		),
 	),
 );
+
+Customizer::add_section(
+	'navigation',
+	'Navigation',
+	'Settings for navigation',
+	array(
+		'first_level_submenu_link'   => array(
+			'type'    => 'checkbox',
+			'label'   => 'First level submenu is link',
+			'default' => false,
+		),
+		'topbar'   => array(
+			'type'    => 'checkbox',
+			'label'   => 'Show topbar',
+			'default' => false,
+		),
+		'scroll' => array(
+			'type'    => 'checkbox',
+			'label'   => 'Scroll',
+			'default' => false,
+		),
+		'sticky' => array(
+			'type'    => 'checkbox',
+			'label'   => 'sticky',
+			'default' => false,
+		),
+		'spacer' => array(
+			'type'    => 'checkbox',
+			'label'   => 'spacer',
+			'default' => false,
+		),
+		'search' => array(
+			'type'    => 'checkbox',
+			'label'   => 'search',
+			'default' => false,
+		),
+	),
+);
