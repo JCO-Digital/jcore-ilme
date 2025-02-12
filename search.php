9ead524af75b937a0b329ee63ad3fb5bae40da25
Niklas Schönberg <niklas@uplink.fi>
2025-02-12 09:30:52 +0200

    feat: search functionality

--- search.php
+++ search.php
@@ -7,18 +7,17 @@
 use Timber\PostQuery;
 use Timber\Timber;
 
-$templates[] = 'search.twig';
-$templates[] = 'archive.twig';
-$templates[] = 'index.twig';
+$templates = array(
+	'search.twig',
+	'archive.twig',
+	'index.twig',
+);
 
 $context = Timber::context();
 
 // Get posts from default query.
 global $wp_query;
-
-$posts = Timber::get_posts( $wp_query );
-
-$context['posts'] = $posts;
+$context['posts'] = Timber::get_posts( $wp_query );
 
 get_header();
 Timber::render( $templates, $context );
