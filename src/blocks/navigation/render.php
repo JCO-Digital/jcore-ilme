92895557e5c227dc56a03c6841c1ffbe14e4304e
Niklas Schönberg <niklas@uplink.fi>
2025-05-02 15:53:01 +0300

    feat: Remove block building from theme.

--- src/blocks/navigation/render.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Timber\Timber;
-
-$context = Timber::context( $attributes );
-
-$context['wrapper_attributes'] = get_block_wrapper_attributes();
-
-Timber::render( 'blocks/navigation.twig', $context );
