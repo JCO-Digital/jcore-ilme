1a58805c1b31532ebe7d6183edc5d063a608b1b6
Lotta <lotta@jco.f>
2025-05-12 15:54:10 +0300

    feat: footer template + parts

---
+++ patterns/footer-logo-grid.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Title: Footer logo grid content (1)
+ * Slug: jcore/footer-logo-grid
+ * Description: Footer logo grid content (first).
+ * Categories: footer
+ * Keywords: footer, logo, footer logo, reusable footer logo row content, reusable pattern
+ * Viewport Width: 640
+ */
+?>
+<!-- wp:group {"metadata":{"name":"Copyright"},"className":"copyright","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group copyright has-black-background-color has-background" style="margin-top:0;margin-bottom:0;padding-right:0;padding-left:0"><!-- wp:jcore/copyright-date {"showStartingYear":true,"startingYear":"2000","companyName":"Netprofile, all rights reserved.","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"mini"} /-->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"mini"} -->
+<p class="has-white-color has-text-color has-link-color has-mini-font-size"><a href="https://kehitys.localhost/another-grid-test/" data-type="page" data-id="3783">Tietosuojaseloste</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
