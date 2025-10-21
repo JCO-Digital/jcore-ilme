9fff41737e1f0e5a86e8aa95deb1099fb0b6d562
Niklas Schönberg <niklas@uplink.fi>
2025-10-21 16:54:44 +0300

    fix: tease-content.twig

--- includes/admin.php
+++ includes/admin.php
@@ -1,18 +1,28 @@
 <?php
-
 /**
  * Load scripts for admin pages.
  *
  * @param string $hook Hook.
  *
  * @return void
+ *
+ * @package Jcore\Ilme
  */
 
 namespace Jcore\Ilme;
 
 use Jcore\Ydin\WordPress\Assets;
 
+/**
+ * Enqueue admin styles.
+ *
+ * @param string $hook Hook.
+ *
+ * @return void
+ */
 function admin_scripts( $hook ) {
 	Assets::style_register( 'jcore-admin-style', '/dist/css/admin.css', array(), '' );
 	wp_enqueue_style( 'jcore-admin-style' );
 }
+
+add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\\admin_scripts' );
